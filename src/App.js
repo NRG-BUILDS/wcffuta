c3721ae7d083e57971526abc102811d8551671d5
Emmanuelomolaju <emmanuelomolaju964@gmail.com>
2023-06-22 14:55:28 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './views/Homepage/Home';
 function App() {
 
