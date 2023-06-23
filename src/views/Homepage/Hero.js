3cea209187e629ec61d617c7200e8d195b10a2fd
Emmanuel Omolaju <111093908+NRG-BUILDS@users.noreply.github.com>
2023-06-23 10:21:19 +0100

    Update Hero.js

--- src/views/Homepage/Hero.js
+++ src/views/Homepage/Hero.js
@@ -3,7 +3,7 @@ import logo from '../../assets/images/wcfLogoLight.png'
 const Hero = () => {
     return ( 
         <header className="hero bg-deepRed text-white flex items-center absolute top-0 pb-10 h-screen w-full">
-            <div className=" max-w-md mx-auto px-8 relative z-20">
+            <div className=" max-w-md mx-auto px-8 relative z-40">
                 <img src={logo} alt="" className=' w-28 h-28'/>
                 <h1 className="font-light text-4xl">Winners Campus Fellowship, FUTA</h1>
                 <p className=" italic">...my destiny moulding place</p>
