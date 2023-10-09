048a9641100e54fed7a64fe088b536fadc190104
Emmanuel Omolaju <111093908+NRG-BUILDS@users.noreply.github.com>
2023-10-09 14:44:14 +0100

    Update MagSect.js

--- src/views/PublicityPage/MagSect.js
+++ src/views/PublicityPage/MagSect.js
@@ -57,10 +57,10 @@ const MagSect = () => {
                     link={'https://drive.google.com/file/d/150pgqyMtfqzr1v7KVrCQuEJF8ftOnNUp/view?usp=drivesdk'}
                     pic={'vol3'}/>
                 <MagCard 
-                    name={'WCF Magazine Vol. 3'}
+                    name={'WCF Magazine Vol. 4'}
                     desc={'Back Better than Ever!'}
                     link={'https://drive.google.com/file/d/1Zh2vEhBHrXxqULJEgWt1Rt8h4ZRQY0li/view?usp=drivesdk'}
-                    pic={'vol3'}/>
+                    pic={'vol4'}/>
             </div>
         </section>
      );
